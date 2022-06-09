b047eab39530c5e2831182b2c42a6e078717e538
@n@ <anacoroletchi@yahoo.com>
2022-06-09 13:57:24 +0300

    Update lab5.py

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -1,31 +1,4 @@
-class TreeNode:
-    def __init__(self, data):
-        self.data = data
-        self.children = []
-        self.parent = None
-
-    def add_child(self, child):
-        child.parent = self
-        self.children.append(child)
-
-    def getLevel(self):
-        p = self.parent
-        level = 0
-        while p:
-            level = level + 1
-            p = p.parent
-        return level
-
-    def printTree(self):
-        spaces = " " * self.getLevel() * 2
-        spaces = spaces + "|__" if self.parent else ""
-        print(spaces + self.data)
-        if self.children:
-            for child in self.children:
-                child.printTree()
-        pass
-
-
+starts = []
 Grammar = [
     ["S", "Ae"],
     ["A", "baB"],
@@ -35,120 +8,212 @@ Grammar = [
     ["D", "c"]
 ]
 
-
 string = "bacbcbcde"
 
-
+mydict = {}
 first = {}
 last = {}
-actions = []
-mydict = {}
-precedence = []
-prior = {}
-tree = []
+equal = []
+lessthan = []
+greaterthan = []
 
 
-def getAllfirst(myelem, already=[]):
-    mylist = []
-    for elmt in mydict[myelem]:
-        first = elmt[0]
-        if not first in already:
-            if first.isupper():
-                already.append(first)
-                mylist.append(first)
-                mylist += getAllfirst(first, already)
-            else:
-                mylist += [first]
-    return mylist
+def getAllfirst(myelem, already="", upper=""):
+    if myelem.isupper():
+        if not myelem in upper:
+            upper += myelem
+            already += myelem
+            if myelem in mydict:
+                for elm in mydict[myelem]:
+                    if not elm in upper and not elm in already:
+                        first = elm[0]
+                        already += "," + getAllfirst(first, "", upper)
+                    else:
+                        return ""
+        else:
+            return ""
+    else:
+        if not myelem in already:
+            already += myelem
+    return already
 
 
-def getAllLast(myelem, already=[]):
-    mylist = []
-    for elmt in mydict[myelem]:
-        for i, first in enumerate(elmt):
-            if first.isupper() and not first in already:
-                already.append(first)
-                mylist += getAllLast(first, already)
-            elif not first in already:
-                already.append(first)
-                mylist += [first]
-    return mylist
+def getAllLast(myelem, already="", upper=""):
+    if myelem.isupper():
+        if not myelem in upper:
+            upper += myelem
+            already += myelem
+            if myelem in mydict:
+                for elm in mydict[myelem]:
+                    if not elm in upper and not elm in already:
+                        first = elm[-1]
+                        already += "," + getAllfirst(first, "")
+                    else:
+                        return ""
+        else:
+            return ""
+    else:
+        if not myelem in already:
+            already += myelem
+    return already
 
+# writes the string in the according way with signs 
+def parse(string):
+    string = "$" + string + "$"
+    s = ""
+    for i in range(len(string)-1):
+        if [string[i], string[i+1]] in equal:
+            s += "="+string[i+1]
+        elif [string[i], string[i+1]] in lessthan or string[i] == "$":
+            s += "<"+string[i+1]
+        elif [string[i], string[i+1]] in greaterthan or string[i+1] == "$":
+            s += ">"+string[i+1]
+        else:
+            s += string[i+1]
+    s = "$"+ s
+    return s
 
-def parseString(text, priority):
-    allNodes = {}
-    while(len(text) > 2):
-        for elm in priority:
-            if elm[1] in text:
-                text = text.replace(elm[1], elm[0], 1)
-                print(text, "  ", f"{elm[0]} --> {elm[1]}")
-                flag = False
-                for chr in elm[1]:
-                    if chr.isupper() and chr in allNodes:
-                        flag = True
+# for example CbD will be changed with C
+def checkRule(param):
+    for lists in Grammar:
+        if param == lists[1]:
+            return lists[0]
+    pass
 
-                if not flag:
-                    node = TreeNode(elm[1])
-                    parentNode = TreeNode(elm[0])
-                    parentNode.add_child(node)
-                    allNodes[elm[0]] = parentNode
-                else:
-                    parentNode = TreeNode(elm[0])
-                    for chr in elm[1]:
-                        if chr.isupper() and chr in allNodes:
-                            parentNode.add_child(allNodes[chr])
-                        else:
-                            node = TreeNode(chr)
-                            parentNode.add_child(node)
+# checks for the <>, changes the start
+def check(ss):
+    flag = False
+    start = -1
+    end = -1
+    for i in range(0, len(ss) - 1):
+        if ss[i] == "<":
+            flag = True
+            start = i+1
+        elif flag and ss[i] == ">":
+            end = i
+            flag = False
+            break
 
-                    allNodes[elm[0]] = parentNode
-    return parentNode
+    # puts = in the initially form like CbD, then changes it into C
+    Replacerules = checkRule(ss[start:end].replace("=", ""))
+
+    # write the form, but with no signs from the table
+    ss = ss[:start] + Replacerules + ss[end:]
+    return ss
+
+# puts the signs in left and right from the table 
+def rerule(ss):
+    for i in range(0, len(ss) - 1):
+        if ss[i] == "<" or ss[i] == ">" or ss[i] == "=":
+            if not(ss[i-1] == "$" or ss[i+1] == "$"):
+                a = "=" if [ss[i-1], ss[i+1]] in equal else ""
+                b = "<" if [ss[i-1], ss[i+1]] in lessthan else ""
+                c = ">" if [ss[i-1], ss[i+1]] in greaterthan else ""
+
+                if len(a) > 0 and not ss[i] == "=":
+                    ss = ss[:i] + a + ss[i + 1:]
+                elif len(b) > 0 and not ss[i] == "<":
+                    ss = ss[:i] + b + ss[i + 1:]
+                elif len(c) > 0 and not ss[i] == ">":
+                    ss = ss[:i] + c + ss[i + 1:]
+    return ss
 
 
 if __name__ == "__main__":
-
     # Converting array to dictionary
+    letters = []
     for elm in Grammar:
+        for el in [el for x in elm for el in x]:
+            if not el in letters:
+                letters.append(el)
+        if not elm[0] in starts:
+            starts.append(elm[0])
         if elm[0] in mydict:
             mydict[elm[0]].append(elm[1])
         else:
             mydict[elm[0]] = [elm[1]]
 
+    # Sorting all Letters
+    letters = sorted(letters)
+    for i, l in enumerate(letters):
+        if l == "S":
+            s = letters.pop(i)
+            letters.insert(0, s) #set S on 0 position, first
+ 
+
     for elm in Grammar:
         myelem = elm[0]
         myelemFirst = elm[1][0]
-        myelemLast = elm[1][1:]
-        first[myelem] = getAllfirst(myelem, [])
-        last[myelem] = getAllLast(myelem, [])
+        myelemLast = elm[1][-1]
+        first[myelem] = getAllfirst(myelemFirst, "")
+        last[myelem] = getAllLast(myelemLast, "")
 
-    # Assigning priority of all Staters/Nonterminal to 0
-    for key in mydict:
-        prior[key] = 0
+    print("\n \tFIRST\t\tLAST".expandtabs(10))
+    for elm in starts:
+        print(f"{elm}\t{first[elm]}\t\t{last[elm]}".expandtabs(10))
 
-    pr = []
-    priority = []
-    for el in Grammar:
-        if len(el[1]) == 1 and el[1].islower():
-            pr.append(el)
+    print("\n")
+    print("Rule1: X1", "=", "X2")
+    for elm in mydict:
+        for val in mydict[elm]:
+            if len(val) > 1:
+                for i in range(0, len(val)-1):
+                    if not [val[i], val[i+1]] in equal:
+                        equal.append([val[i], val[i+1]])
+                        print(val[i], "=", val[i+1])
 
-    StartingPoint = "S"
-    el = mydict["S"][0]
-    lowpr = []
+    print("\n")
+    print("Rule2: X1", "<", "X2" )
+    for elm in mydict:
+        for val in mydict[elm]:
+            if len(val) > 1:
+                for i in range(0, len(val)-1):
+                    if not [val[i], val[i+1]] in lessthan and val[i].islower() and val[i+1].isupper():
+                        print(val[i], " <   First(", val[i+1], ") => {", first[val[i+1]].strip(), "}")
+                        for el in first[val[i+1]].strip().split(","):
+                            if len(el.strip()) > 0:
+                                lessthan.append([val[i], el])
 
-    while not [StartingPoint, el] in pr:
-        for elm in mydict[StartingPoint]:
-            lowpr.append([StartingPoint, elm])
-        for elm in mydict[StartingPoint]:
-            for chr in elm:
-                if chr.isupper():
-                    el = mydict[chr][0]
-                    StartingPoint = chr
+    print("\n")
+    print("Rule3: X1", ">", "X2" )
+    for elm in mydict:
+        for val in mydict[elm]:
+            if len(val) > 1:
+                for i in range(0, len(val)-1):
+                    if not [val[i], val[i+1]] in greaterthan and val[i].isupper() and val[i+1].islower():
+                        print("Last(", val[i], ")  >  ", val[i+1], "=> {", last[val[i]].strip(), "}   >  ", val[i+1])
+                        for el in last[val[i]].strip().split(","):
+                            if len(el.strip()) > 0:
+                                greaterthan.append([el, val[i+1]])
 
-    priority = pr
+    table = {}
+    letters.append("$")
+    s = "   "
+    for item in letters:
+        s = s+item + "\t"
+        for item2 in letters:
+            table[item+item2] = "=" if [item, item2] in equal else "<" if [item, item2] in lessthan else ">" if [
+                item, item2] in greaterthan else "<" if item == "$" else ">" if item2 == "$" else ""
+            
+    print("\n")
+    print(s.expandtabs(6))
 
-    for el in reversed(lowpr):
-        priority.append(el)
-    print(priority)
-
-    root = parseString(string, priority)
-    root.printTree()
+    for x, item in enumerate(letters):
+        s = " "
+        for y, item2 in enumerate(letters):
+          s += table[item + item2] + "\t"
+        print(item + s.expandtabs(6),"\n")
+    print(f"String {string} \n")
+    ss = parse(string)
+    print(ss)
+    parsedFlag = True
+    for i in range(0, 100):
+        ss = check(ss)
+        ss = rerule(ss)
+        print(ss)
+        if ss == "$<S>$":
+            print("\n","String is accepted")
+            parsedFlag = False
+            break
+    if parsedFlag:
+        print("The string is not accepted")
